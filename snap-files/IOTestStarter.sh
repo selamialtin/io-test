f12032f5d6d1a412e08ff50692ec295d251572ca
selami <selami.altin@uzmar.net>
2019-10-09 15:17:42 +0300

    snap oyunlari

--- snap-files/IOTestStarter.sh
+++ snap-files/IOTestStarter.sh
@@ -1,6 +1,6 @@
-
+#!/bin/sh
 export JAVA_HOME=$SNAP/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$JAVA_HOME/jre/bin:$PATH
 export PATH=$SNAP/bin/jre:$PATH
 
-java -jar -cp $SNAP/bin/jre-ext/* $SNAP/bin/IOTester.jar
+java -jar -Duser.home=$SNAP_USER_DATA -cp $SNAP/bin/jre-ext/* $SNAP/bin/IOTester.jar
